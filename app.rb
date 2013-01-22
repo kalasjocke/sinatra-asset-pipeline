7c93584e570dfb0b8479d8a5779fab3a6035ef52
Joakim Ekberg <jocke.ekberg@gmail.com>
2013-01-22 15:43:19 +0100

    Move all asset related code into it's own module

--- app.rb
+++ app.rb
@@ -1,38 +1,55 @@
 require 'bundler'
 Bundler.require
 
+module Sinatra
+  module AssetPipeline
+    def self.registered(app)
+      app.set :sprockets, Sprockets::Environment.new
+      app.set :assets_prefix, '/assets'
+      app.set :assets_path, -> { File.join(public_folder, assets_prefix) }
+      app.set :static, true
+      app.set :static_cache_control, [:public, :max_age => 525600]
+
+      app.configure do
+        %w{images javascripts stylesheets}.each do |thing|
+          app.sprockets.append_path "assets/#{thing}"
+        end
+
+        Sprockets::Helpers.configure do |config|
+          config.environment = app.sprockets
+          config.digest = true
+        end
+      end
+
+      app.configure :staging, :production do
+        Sprockets::Helpers.configure do |config|
+          config.manifest = Sprockets::Manifest.new(app.sprockets, App.assets_path)
+        end
+      end
+
+      app.configure :production do
+        Sprockets::Helpers.configure do |config|
+          config.protocol = 'http'
+          config.asset_host = 'id.cloudfront.net'
+        end
+      end
+
+      app.helpers Sprockets::Helpers
+
+      app.configure :development do
+        app.get '/assets/*' do |key|
+          key.gsub!(/(-\w+)/, "")
+          asset = app.sprockets[key]
+          content_type asset.content_type
+          asset.to_s
+        end
+      end
+    end
+  end
+end
+
 class App < Sinatra::Base
-  set :sprockets, Sprockets::Environment.new
-  set :assets_prefix, '/assets'
-  set :assets_path, -> { File.join(public_folder, assets_prefix) }
-  set :static, true
-  set :static_cache_control, [:public, :max_age => 525600]
-
-  configure do
-    %w{images javascripts stylesheets}.each do |thing|
-      sprockets.append_path "assets/#{thing}"
-    end
-
-    Sprockets::Helpers.configure do |config|
-      config.environment = sprockets
-      config.digest = true
-    end
-  end
-
-  configure :staging, :production do
-    Sprockets::Helpers.configure do |config|
-      config.manifest = Sprockets::Manifest.new(sprockets, App.assets_path)
-    end
-  end
-
-  configure :production do
-    Sprockets::Helpers.configure do |config|
-      config.protocol = 'http'
-      config.asset_host = 'id.cloudfront.net'
-    end
-  end
-
-  helpers Sprockets::Helpers
+  register Sinatra::AssetPipeline
 
   get '/' do
     haml :index
--- config.ru
+++ config.ru
@@ -5,10 +5,6 @@ use Rack::Deflater
 configure :development do
   require 'rack-livereload'
   use Rack::LiveReload
-
-  map App.assets_prefix do
-    run App.sprockets
-  end
 end
 
 run App
