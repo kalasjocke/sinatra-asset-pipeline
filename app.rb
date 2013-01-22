c6d6b73265030f4a20fa7da0a814718fbabe552b
Joakim Ekberg <jocke.ekberg@gmail.com>
2013-01-22 14:51:26 +0100

    Move Sprockets setup into the application

--- app.rb
+++ app.rb
@@ -1,6 +1,39 @@
-require 'sinatra'
+require 'bundler'
+Bundler.require
 
 class App < Sinatra::Base
+  set :sprockets, Sprockets::Environment.new
+  set :assets_prefix, '/assets'
+  set :assets_path, -> { File.join(public_folder, assets_prefix) }
+  set :static, true
+  set :static_cache_control, [:public, :max_age => 525600]
+
+  configure do
+    %w{images javascripts stylesheets}.each do |thing|
+      sprockets.append_path "assets/#{thing}"
+    end
+
+    Sprockets::Helpers.configure do |config|
+      config.environment = sprockets
+      config.digest = true
+    end
+  end
+
+  configure :staging, :production do
+    Sprockets::Helpers.configure do |config|
+      config.manifest = Sprockets::Manifest.new(sprockets, App.assets_path)
+    end
+  end
+
+  configure :production do
+    Sprockets::Helpers.configure do |config|
+      config.protocol = 'http'
+      config.asset_host = 'id.cloudfront.net'
+    end
+  end
+
+  helpers Sprockets::Helpers
+
   get '/' do
     haml :index
   end
--- config.ru
+++ config.ru
@@ -1,32 +1,14 @@
-require 'bundler'
-Bundler.require
-
 require './app'
 
 use Rack::Deflater
 
-if ENV["RACK_ENV"] == "development"
+configure :development do
   require 'rack-livereload'
   use Rack::LiveReload
-end
 
-map '/assets' do
-  environment = Sprockets::Environment.new
-
-  %w{images javascripts stylesheets}.each do |thing|
-    environment.append_path "assets/#{thing}"
+  map App.assets_prefix do
+    run App.sprockets
   end
-
-  Sprockets::Helpers.configure do |config|
-    config.environment = environment
-    config.digest = true
-  end
-
-  App.class_eval do
-    helpers Sprockets::Helpers
-  end
-
-  run environment
 end
 
 run App
