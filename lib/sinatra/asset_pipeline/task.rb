c351ae0c169b6b4f7fc100f9e2f619ce7605e617
Joakim Ekberg <jocke.ekberg@gmail.com>
2013-03-01 16:13:07 +0100

    Rake tasks for precompiling and cleaning assets

---
+++ lib/sinatra/asset_pipeline/task.rb
@@ -0,0 +1,29 @@
+require 'rake'
+require 'rake/tasklib'
+require 'rake/sprocketstask'
+
+module Sinatra
+  module AssetPipeline
+    class Task < Rake::TaskLib
+      def initialize(app)
+        namespace :assets do
+          desc "Precompile assets"
+          task :precompile do
+            environment = app.sprockets
+            manifest = Sprockets::Manifest.new(environment.index, app.assets_path)
+            manifest.compile(app.assets_precompile)
+          end
+
+          desc "Clean assets"
+          task :clean do
+            FileUtils.rm_rf(app.assets_path)
+          end
+        end
+      end
+
+      def self.define!(app)
+        self.new app
+      end
+    end
+  end
+end
