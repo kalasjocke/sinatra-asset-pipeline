806ac95c88424554d0f38ddf9362a0153289dcfe
Joakim Ekberg <jocke.ekberg@gmail.com>
2013-10-19 22:34:06 +0200

    Refactor app to use a more descriptive name

--- lib/sinatra/asset_pipeline/task.rb
+++ lib/sinatra/asset_pipeline/task.rb
@@ -5,24 +5,24 @@ require 'rake/sprocketstask'
 module Sinatra
   module AssetPipeline
     class Task < Rake::TaskLib
-      def initialize(app)
+      def initialize(app_klass)
         namespace :assets do
           desc "Precompile assets"
           task :precompile do
-            environment = app.sprockets
-            manifest = Sprockets::Manifest.new(environment.index, app.assets_path)
-            manifest.compile(app.assets_precompile)
+            environment = app_klass.sprockets
+            manifest = Sprockets::Manifest.new(environment.index, app_klass.assets_path)
+            manifest.compile(app_klass.assets_precompile)
           end
 
           desc "Clean assets"
           task :clean do
-            FileUtils.rm_rf(app.assets_path)
+            FileUtils.rm_rf(app_klass.assets_path)
           end
         end
       end
 
-      def self.define!(app)
-        self.new app
+      def self.define!(app_klass)
+        self.new app_klass
       end
     end
   end
