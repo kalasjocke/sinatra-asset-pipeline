34289aad6f2666f28cff7695227e675622f4499b
Joakim Ekberg <jocke.ekberg@gmail.com>
2013-10-19 22:48:06 +0200

    Refactor assets_prefix to use a shorter syntax

--- lib/sinatra/asset_pipeline.rb
+++ lib/sinatra/asset_pipeline.rb
@@ -7,7 +7,7 @@ module Sinatra
     def self.registered(app)
       app.set_default :sprockets, Sprockets::Environment.new
       app.set_default :assets_precompile, %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff)
-      app.set_default :assets_prefix, %w[assets vendor/assets]
+      app.set_default :assets_prefix, %w(assets vendor/assets)
       app.set_default :assets_path, -> { File.join(public_folder, "assets") }
       app.set_default :assets_protocol, :http
       app.set_default :assets_css_compressor, :none
@@ -18,7 +18,7 @@ module Sinatra
       app.set :static_cache_control, [:public, :max_age => 60 * 60 * 24 * 365]
 
       app.configure do
-        Array(app.assets_prefix).each do |prefix|
+        app.assets_prefix.each do |prefix|
           Dir[File.join prefix, "*"].each { |path| app.sprockets.append_path path }
         end
         Sprockets::Helpers.configure do |config|
