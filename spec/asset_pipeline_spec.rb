117c4841e1bb8bb5ecc0310911093465361a72ca
Ole Michaelis <Ole.Michaelis@googlemail.com>
2014-08-04 23:43:01 +0200

    fixed tests also switched rspec deprecated syntax to new expect syntax

--- spec/asset_pipeline_spec.rb
+++ spec/asset_pipeline_spec.rb
@@ -5,53 +5,53 @@ describe Sinatra::AssetPipeline do
 
   describe App do
     describe "assets_precompile" do
-      it { App.assets_precompile.should eq %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff) }
+      it { expect(App.assets_precompile).to eq %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff) }
     end
 
     describe "assets_prefix" do
-      it { App.assets_prefix.should eq %w(spec/assets) }
+      it { expect(App.assets_prefix).to eq %w(spec/assets) }
     end
 
     describe "assets_host" do
-      it { App.assets_host.should eq nil }
+      it { expect(App.assets_host).to eq nil }
     end
 
     describe "assets_protocol" do
-      it { App.assets_protocol.should eq :http }
+      it { expect(App.assets_protocol).to eq :http }
     end
 
     describe "assets_css_compressor" do
-      it { App.sprockets.css_compressor.should eq nil }
+      it { expect(App.sprockets.css_compressor).to eq nil }
     end
 
     describe "assets_js_compressor" do
-      it { App.sprockets.js_compressor.should eq nil }
+      it { expect(App.sprockets.js_compressor).to eq nil }
     end
   end
 
   describe CustomApp do
     describe "assets_precompile" do
-      it { CustomApp.assets_precompile.should eq %w(foo.css foo.js) }
+      it { expect(CustomApp.assets_precompile).to eq %w(foo.css foo.js) }
     end
 
     describe "assets_prefix" do
-      it { CustomApp.assets_prefix.should eq %w(spec/assets, foo/bar) }
+      it { expect(CustomApp.assets_prefix).to eq %w(spec/assets, foo/bar) }
     end
 
     describe "assets_host" do
-      it { CustomApp.assets_host.should eq 'foo.cloudfront.net' }
+      it { expect(CustomApp.assets_host).to eq 'foo.cloudfront.net' }
     end
 
     describe "assets_protocol" do
-      it { CustomApp.assets_protocol.should eq :https }
+      it { expect(CustomApp.assets_protocol).to eq :https }
     end
 
     describe "assets_css_compressor" do
-      it { CustomApp.sprockets.css_compressor.should eq Sprockets::SassCompressor }
+      it { expect(CustomApp.sprockets.css_compressor).to eq Sprockets::SassCompressor }
     end
 
     describe "assets_js_compressor" do
-      it { CustomApp.sprockets.js_compressor.should eq Sprockets::UglifierCompressor }
+      it { expect(CustomApp.sprockets.js_compressor).to eq Sprockets::UglifierCompressor }
     end
   end
 
@@ -64,19 +64,19 @@ describe Sinatra::AssetPipeline do
 
     it "serves an asset" do
       get '/assets/test-_foo.css'
-      last_response.should be_ok
-      last_response.body.should eq css_content
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq css_content
     end
 
     it "serves an asset with a digest filename" do
       get '/assets/constructocat2-b5921515627e82a923079eeaefccdbac.jpg'
-      last_response.should be_ok
+      expect(last_response).to be_ok
     end
 
     it "serves only the asset body with query param body=1" do
       get '/assets/test_body_param.js?body=1'
-      last_response.should be_ok
-      last_response.body.should eq %Q[var str = "body";\n]
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq %Q[var str = "body";\n]
     end
   end
 end
