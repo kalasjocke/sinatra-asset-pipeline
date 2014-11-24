f948d063aea76a3b2a9528b5da4e8fd4f51cf0e1
Joakim Ekberg <jocke.ekberg@gmail.com>
2014-11-24 21:36:54 +0100

    Fix newlines

--- spec/asset_pipeline/task_spec.rb
+++ spec/asset_pipeline/task_spec.rb
@@ -14,9 +14,9 @@ describe Sinatra::AssetPipeline::Task do
       expect(File.exists?(Dir.glob("public/assets/manifest-*.json").first)).to be true
 
       manifest = JSON.parse File.read(Dir.glob("public/assets/manifest-*.json").first)
+
       manifest["files"].each_key do |file|
         expect(File.exists?("public/assets/#{file}")).to be true
-
         expect(File.read("public/assets/#{file}")).to eq js_content  if file.end_with? '.js'
         expect(File.read("public/assets/#{file}")).to eq css_content if file.end_with? '.css'
       end
