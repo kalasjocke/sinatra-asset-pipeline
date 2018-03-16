39bdbf9f7875250cc30000f3268e7971ab90a27c
msalzburg <salzburg@emjot.de>
2018-03-16 18:10:09 +0100

    Use SprocketsTask to enhance rake task

--- spec/asset_pipeline/task_spec.rb
+++ spec/asset_pipeline/task_spec.rb
@@ -7,18 +7,20 @@ describe Sinatra::AssetPipeline::Task do
 
   before(:all) { Dir.chdir "spec" }
 
+  let(:manifest) do
+    manifest_path = 'public/assets/.sprockets-manifest-*.json'
+    globbed = Dir.glob(manifest_path)
+    JSON.parse File.read(globbed.first)
+  end
+
   describe "assets:precompile" do
+    before { Rake::Task['assets:precompile'].invoke }
+
+    it "generates a manifest" do
+      expect(manifest).not_to be_empty
+    end
+
     it "precompiles assets" do
-      Rake::Task['assets:precompile'].invoke
-
-      manifest_path = 'public/assets/.sprockets-manifest-*.json'
-      globbed = Dir.glob(manifest_path)
-
-      expect(globbed).to_not be_empty
-      expect(File.exists?(globbed.first)).to be true
-
-      manifest = JSON.parse File.read(globbed.first)
-
       manifest["files"].each_key do |file|
         expect(File.exists?("public/assets/#{file}")).to be true
         expect(File.read("public/assets/#{file}")).to eq js_content  if file.end_with? '.js'
@@ -28,9 +30,23 @@ describe Sinatra::AssetPipeline::Task do
   end
 
   describe "assets:clean" do
-    it "cleans precompiled assets" do
-      Rake::Task['assets:clean'].invoke
+    before { Rake::Task['assets:precompile'].invoke }
 
+    context 'with default keep of 2' do
+      it "cleans precompiled assets" do
+        Rake::Task['assets:clean'].invoke
+
+        expect(Dir['public/assets']).not_to be_empty
+      end
+    end
+  end
+
+  describe "assets:clopper" do
+    before { Rake::Task['assets:precompile'].invoke }
+
+    it 'removes all compiled assets' do
+      expect(Dir['public/assets']).not_to be_empty
+      Rake::Task['assets:clobber'].invoke
       expect(Dir['public/assets']).to be_empty
     end
   end
